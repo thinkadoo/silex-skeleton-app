1e63671c9ad2ba50932e82a572bed723d10201f2
Andre Venter <andre.n.venter@gmail.com>
2014-01-23 14:56:34 +0200

    add relative path for travis and xdebug monolog file output

--- app/config/test.php
+++ app/config/test.php
@@ -4,7 +4,7 @@ $app['debug'] = true;
 $app['exception_handler']->disable();
 
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__.'/../../logs/testing.log',
+    'monolog.logfile' => './logs/testing.log',
 ));
 
 $app['monolog']->addDebug('Testing the Monolog logging from /config/test.php ');
