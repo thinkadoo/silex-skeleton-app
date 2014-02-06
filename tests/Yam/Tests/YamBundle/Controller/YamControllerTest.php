2be86bb8b938c629556d485190dcb902b8099b59
Andre Venter <andre.n.venter@gmail.com>
2014-02-06 20:15:14 +0200

    add testController and scaffold both Bundles and Tests

---
+++ tests/Yam/Tests/YamBundle/Controller/YamControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * File: YamControllerTest.php
+ *
+ * PHP Version 5.3.21
+ *
+ * @category Api_Rest_Implementation_Tests
+ * @package  Modules_Yam
+ * @author   Andre Venter <andre.n.venter@gmail.com>
+ * @license  Thinkadoo http://think-a-doo.net
+ * @link     https://github.com/thinkadoo/silex-skeleton-rest.git
+ */
+namespace Yam\Tests\YamBundle\Controller;
+
+use Silex\WebTestCase;
+
+/**
+ * Class YamController
+ *
+ * @category Api_Rest_Implementation_Tests
+ * @package  Yam\YamBundle\Controller
+ * @author   Andre Venter <andre.n.venter@gmail.com>
+ * @license  Thinkadoo http://think-a-doo.net
+ * @link     https://github.com/thinkadoo/silex-skeleton-rest.git
+ */
+class YamControllerTest extends WebTestCase
+{
+    /**
+     * createApplication
+     *
+     * @return mixed|\Symfony\Component\HttpKernel\HttpKernel
+     */
+    public function createApplication()
+    {
+        return include $_SERVER['APP_DIR'] . "/app_test.php";
+    }
+
+    /**
+     * testInitialPage
+     *
+     * @return void
+     */
+    public function testInitialPage()
+    {
+        $this->assertEquals(1,1);
+    }
+}
+/* End of file YamControllerTest.php */
