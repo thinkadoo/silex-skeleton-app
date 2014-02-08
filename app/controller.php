71c42307ada88c82b49fbec03a130a8760da5860
Andre Venter <andre.n.venter@gmail.com>
2014-02-08 13:42:18 +0200

    add Bootstrap files and App Controller file scaffolding and remove all bundles and tests

--- app/controller.php
+++ app/controller.php
@@ -1,10 +1,5 @@
 <?php
 
-$app->mount('/yam', new Yam\YamBundle\Controller\YamController());
-$app->mount('/yoo', new Yoo\YooBundle\Controller\YooController());
-$app->mount('/yum', new Yum\YumBundle\Controller\YumController());
-$app->mount('/user', new User\UserBundle\Controller\UserController());
-
 $app->get('/', function () {
-    return "Welcome To ReSTful API";
+return "Welcome To ReSTful API";
 });
---
+++ testbuilder/Bob/TestsBootstrapBuilder.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bob;
+
+use TwigGenerator\Builder\BaseBuilder;
+
+class TestsBootstrapBuilder extends BaseBuilder
+{
+}
