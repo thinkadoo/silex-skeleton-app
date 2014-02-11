46ce57f2c562fe50ae5d70a989f9be922df70816
Andre Venter <andre.n.venter@gmail.com>
2014-02-11 06:55:47 +0200

    add doctrine migrations to "db"

---
+++ db/resttestdb/migrations-db.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'driver'    => 'pdo_mysql',
+    'host'      => '127.0.0.1:8889',
+    'user'      => 'root',
+    'password'  => 'root',
+    'dbname'    => 'restdb'
+);
