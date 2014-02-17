ae76ec317538118e8a28fb2137fcccb02d6d2da4
Andre Venter <andre.n.venter@gmail.com>
2014-02-17 18:13:44 +0200

    set migration passwords same as others

--- db/resttestdb/migrations-db.php
+++ db/resttestdb/migrations-db.php
@@ -6,6 +6,6 @@ return array(
     'driver'    => 'pdo_mysql',
     'host'      => '127.0.0.1:8889',
     'user'      => 'root',
-    'password'  => 'root',
+    'password'  => '',
     'dbname'    => 'resttestdb'
 );
