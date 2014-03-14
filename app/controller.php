9d33700800fecc5e56c67d1b68fd76a2a6ae850e
Andre Venter <andre.n.venter@gmail.com>
2014-03-14 21:04:52 +0200

    more cleanup

--- app/controller.php
+++ app/controller.php
@@ -1,7 +1,5 @@
 <?php
 
-$app->mount('/user', new User\UserBundle\Controller\UserController());
-
 $app->get('/', function () use ($app) {
     return $app['twig']->render('welcome.twig', array());
 });
