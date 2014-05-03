56e9a2fb8091676754eae01a9a5b33e868d160b6
Andre Venter <andre.n.venter@gmail.com>
2014-05-03 10:01:55 +0200

    match migrations db authentication to rest of the project

--- db/restdb/migrations-db.php
+++ db/restdb/migrations-db.php
@@ -7,6 +7,6 @@ return array(
     'host'      => '127.0.0.1',
     'port'      => '3306',
     'user'      => 'root',
-    'password'  => 'root',
+    'password'  => '',
     'dbname'    => 'restdb'
 );
