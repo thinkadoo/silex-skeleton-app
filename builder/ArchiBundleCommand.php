5cdcef3e3614f6df638f5be2521dc4d668b54353
Andre Venter <andre@cashkows.com>
2015-07-22 11:51:49 +0200

    add console command for Archi

---
+++ builder/ArchiBundleCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Builder;
+
+require_once 'loader.php';
+
+use Silex\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use PDO;
+
+class ArchiBundleCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('generate:archibundle')
+            ->setDescription('Generate a Mobile app from Archi Data')
+            ->addArgument(
+                'archiModelFolder',
+                InputArgument::REQUIRED,
+                'What rest class do you want to generate?'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $archiModelFolder = $input->getArgument('archiModelFolder');
+
+        $reader = new \EasyCSV\Reader($archiModelFolder.'/'.'elements.csv');
+        $elements = $reader->getAll();
+        print_r("elements = \n");
+        print_r($elements);
+
+        $reader = new \EasyCSV\Reader($archiModelFolder.'/'.'relations.csv');
+        $relations = $reader->getAll();
+        print_r("relations = \n");
+        print_r($relations);
+
+        $reader = new \EasyCSV\Reader($archiModelFolder.'/'.'properties.csv');
+        $properties = $reader->getAll();
+        print_r("properties = \n");
+        print_r($properties);
+
+    }
+}
---
+++ builder/loader.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->register();
+
+return $loader;
